60346f84ebc7832be169bf1659fdb0a93dfa9bf9
ojasuno <ojasuno21@gmail.com>
2022-07-05 13:15:13 +1000

    meta.json

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: '.world',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
@@ -61,7 +61,7 @@ export default {
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerEditLink: 'GitHub',
+  footerText: <>MIT {new Date().getFullYear()} © 5115.</>,
   unstable_faviconGlyph: '👋',
 }
