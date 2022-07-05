3fa5fd76357b4dc7ec821744d6b4497a552a8da6
ojasuno <ojasuno21@gmail.com>
2022-07-05 13:17:48 +1000

    meta.json

--- theme.config.js
+++ theme.config.js
@@ -4,9 +4,9 @@ export default {
   titleSuffix: '.world',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">5115</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Build on Solana Mobile Stack (SMS) !!!
       </span>
     </>
   ),
